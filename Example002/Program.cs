6af4d7dcf2eec1ff650cfca8555396b618e3f819
Akmaral <axma.mail@gmail.com>
2022-10-06 19:12:49 +0600

    Функция пример

---
+++ Example002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Функция, процедура
+
+Console.Clear();
+
+int f(int a, int b)   // функция
+{
+    return a + b;  // возвращает данные и завершает работу функции
+}
+
+Console.WriteLine(f(5, 5));
