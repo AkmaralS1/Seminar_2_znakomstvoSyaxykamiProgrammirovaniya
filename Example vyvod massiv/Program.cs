fe0e07e416ac75d8ffb26cf803352df9984f2b27
Akmaral <axma.mail@gmail.com>
2022-10-06 18:36:28 +0600

    Пример вывода массива в терминал

---
+++ Example vyvod massiv/Program.cs
@@ -0,0 +1,10 @@
+﻿// пример вывода массива в терминал
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+        array [i] = new Random().Next(-10, 11);
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");   
+
+// Вывод можно написать по другому: 
+Console.WriteLine($"[{string.Join(", ", array)}]");
