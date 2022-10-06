14b115c2ac0eb078e903a887c18f2217b981d8e8
Akmaral <axma.mail@gmail.com>
2022-10-06 18:54:50 +0600

    Функция, процедура

---
+++ Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Функция, процедура
+// процедурой называется все то что не возвращается
+
+Console.Clear();
+void f(int n)   // процедура
+{
+    n = n + 10;
+}
+
+int n = 5;
+Console.WriteLine(n);
+f(n);
+Console.WriteLine(n);
