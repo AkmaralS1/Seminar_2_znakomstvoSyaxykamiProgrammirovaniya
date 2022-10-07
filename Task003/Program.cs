d76feba3498079511aa7ef67e42f017714a392fa
Akmaral <axma.mail@gmail.com>
2022-10-07 21:37:58 +0600

    Задача 12

---
+++ Task003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 12. На вход второе числа и выводить, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1 то прога выводит остаток от деления
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m ==0)      // если остаток при дилении первого на второе равен 0 то yes 
+    Console.WriteLine("yes"); 
+else
+    Console.WriteLine($"no, ostatok {n % m}");
